e9cd60db7e1174dd9013fa9bdb00e7b266be6731
Caner Korkmaz <info@canerkorkmaz.com>
2017-05-20 15:44:59 +0300

    Started on cmake & project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+// Created by Caner Korkmaz on 20/5/2017.
+// Copyright 2017 Caner Korkmaz
+//
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "Hello from Socket Play\n";
+  return EXIT_SUCCESS;
+}
